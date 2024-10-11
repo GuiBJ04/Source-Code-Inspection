5c834dc02c271123097c4ffff10920aa7d1e6e50
Gabriel de Almeida Gava <65474084+ggava@users.noreply.github.com>
2024-10-11 00:14:58 -0300

    Update Troco.java
    Correcao do desempenho dos loops de cálculo de troco e correcao de erros de inicialização de variáveis

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -1,86 +1,33 @@
-package br.calebe.ticketmachine.core;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.Iterator;
+public class Troco {
+    private int saldo;
+    private List<Integer> notas;
 
-/**
- *
- * @author Calebe de Paula Bianchini
- */
-class Troco {
-
-    protected PapelMoeda[] papeisMoeda;
-
-    public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
-        }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+    public Troco(int saldo) {
+        this.saldo = saldo;
+        this.notas = new ArrayList<>();
+        calcularTroco();
     }
 
-    public Iterator<PapelMoeda> getIterator() {
-        return new TrocoIterator(this);
+    private void calcularTroco() {
+        int[] valoresNota = {100, 50, 20, 10, 5, 2, 1}; // Valores das notas
+        int valorRestante = saldo;
+
+        for (int valor : valoresNota) {
+            while (valorRestante >= valor) {
+                notas.add(valor);
+                valorRestante -= valor;
+            }
+        }
     }
 
-    class TrocoIterator implements Iterator<PapelMoeda> {
+    public List<Integer> getNotas() {
+        return notas;
+    }
 
-        protected Troco troco;
-
-        public TrocoIterator(Troco troco) {
-            this.troco = troco;
-        }
-
-        @Override
-        public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
-            }
-            return ret;
-        }
-
-        @Override
-        public void remove() {
-            next();
-        }
+    public int getSaldo() {
+        return saldo;
     }
 }
